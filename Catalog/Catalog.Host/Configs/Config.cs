e5828e9a2d2f44158ce9a5869efd885aeae272ba
AntSenokosov <a.y.senokosov@gmail.com>
2022-11-14 16:25:31 +0200

    Add configs

---
+++ Catalog/Catalog.Host/Configs/Config.cs
@@ -0,0 +1,10 @@
+namespace Catalog.Host.Configs;
+
+public class Config
+{
+    public string CdnHost { get; set; } = null!;
+
+    public string ImgUrl { get; set; } = null!;
+
+    public string ConnectionString { get; set; } = null!;
+}
