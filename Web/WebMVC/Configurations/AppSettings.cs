b1f91bf70f615e617fa5acbd285bd196c4b3e5bd
AntSenokosov <a.y.senokosov@gmail.com>
2022-11-15 12:57:48 +0200

    Add settings

---
+++ Web/WebMVC/Configurations/AppSettings.cs
@@ -0,0 +1,9 @@
+namespace WebMVC.Configurations;
+
+public class AppSettings
+{
+    public string? CatalogUrl { get; set; }
+    public string? AdminCategoryUrl { get; set; }
+    public string? AdminManufactureUrl { get; set; }
+    public string? AdminProductUrl { get; set; }
+}
